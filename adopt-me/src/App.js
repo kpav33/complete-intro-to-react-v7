f4c09899929f3c14f243096a52d59232ac9351de
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-10-17 11:02:34 +0200

    Add js tools

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,15 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
 // When you have distinct ideas represented as markup, it's a good idea to separate that into a component
-const Pet = (props) => {
-  // Props are variables that parent (App) passes to children (instance of Pet), where each prop can be different
-  // This is useful, because it makes components easily reusable with different data passed
-  // To make element have multiple children pass it an array of elements
-  return React.createElement("div", {}, [
-    // In createElement method the last two parameters are optional, since Pet component has no props or children we could just leave them empty
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+// const Pet = (props) => {
+//   // Props are variables that parent (App) passes to children (instance of Pet), where each prop can be different
+//   // This is useful, because it makes components easily reusable with different data passed
+//   // To make element have multiple children pass it an array of elements
+//   return React.createElement("div", {}, [
+//     // In createElement method the last two parameters are optional, since Pet component has no props or children we could just leave them empty
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
 
 const App = () => {
   return React.createElement("div", {}, [
