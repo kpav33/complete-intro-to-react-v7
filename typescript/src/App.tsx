d0ee02a9da2971e12cc8b6f3fb75489b7973dc88
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-10-21 11:37:43 +0200

    Add final clean up, comments

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
+// Should you use TypeScript?
+// However, now that we're playing TypeScript land, this code would be joyous to work on. Visual Studio Code will autocomplete for you. TypeScript will instantly let you know when you've made a mistake. You can launch new code with higher certainty that you haven't created run time errors. This all comes at the cost of taking longer to write. Ask yourself if that's a trade-off you're willing to make: if you're a tiny startup that may not happen. If you're as large as Microsoft, maybe! It's a trade-off like all things are. It is a question you should answer before you start a new code base: should we type check?
+
 const App = () => {
   const theme = useState("darkblue");
   return (
